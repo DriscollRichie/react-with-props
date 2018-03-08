fc032c0c1f29a1e8aa19a95a0a2456a26694f402
= <webdev.jameslemire@gmail.com>
2018-03-08 09:58:31 -0700

    JL master - Import DisplayString and CaptureString components into App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import './App.css';
 
+import DisplayString from './DisplayString/DisplayString'
+import CaptureString from './CaptureString/CaptureString'
+
 class App extends Component {
   render() {
     return (
