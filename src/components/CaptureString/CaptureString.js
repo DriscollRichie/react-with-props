2ab61ab5e59d39263801b03bb9a955e746233bca
= <webdev.jameslemire@gmail.com>
2018-03-08 09:56:40 -0700

    JL master - Add basic component templates to test if they work.

---
+++ src/components/CaptureString/CaptureString.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class CaptureString extends Component {
+   render () {
+      return (
+         <div id="CaptureString__parent">
+            <span>CaptureString Component</span>
+         </div>
+      )
+   }
+}
---
+++ src/components/DisplayString/DisplayString.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class DisplayString extends Component {
+   render () {
+      return (
+         <div id="DisplayString__parent">
+            <span>DisplayString Component</span>
+         </div>
+      )
+   }
+}
