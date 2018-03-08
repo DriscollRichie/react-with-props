ee8c3519ae87998c633b63ef333904a1f030ff1a
= <webdev.jameslemire@gmail.com>
2018-03-08 10:03:26 -0700

    JL master - Dynamically render string from props or an alternative message.

--- src/components/DisplayString/DisplayString.js
+++ src/components/DisplayString/DisplayString.js
@@ -4,7 +4,13 @@ export default class DisplayString extends Component {
    render () {
       return (
          <div id="DisplayString__parent">
-            <span>DisplayString Component</span>
+            {
+               this.props.string
+               ?
+                  <span>{ this.props.string }</span>
+               :
+                  <span><b>String has not be assigned yet</b></span>
+            }
          </div>
       )
    }
